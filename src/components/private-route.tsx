2cb0ef3ad0e43785b3b9d72432bfc2cdbbc512ac
Demurearr <kuligina98@gmail.com>
2025-06-26 11:34:09 +0500

    маршрут

---
+++ src/components/private-route.tsx
@@ -0,0 +1,13 @@
+import { ReactElement } from 'react';
+import { LoginScreen } from '../pages/login-screen/login-screen.tsx';
+
+interface PrivateRouteProps {
+  isAuthorized?: boolean;
+  children: ReactElement;
+}
+
+export function PrivateRoute(props: PrivateRouteProps): ReactElement {
+  const { isAuthorized, children } = props;
+
+  return isAuthorized ? children : <LoginScreen />;
+}
